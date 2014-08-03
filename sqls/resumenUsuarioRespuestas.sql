0f1f25c89193d83f293335e4b049ceea45f9b893
Ezequiel Telleria <eltelle@gmail.com>
2014-08-03 02:52:32 -0300

    Vista corregida

--- sqls/resumenUsuarioRespuestas.sql
+++ sqls/resumenUsuarioRespuestas.sql
@@ -1,7 +1,22 @@
-create or replace view  encuestas.resumenUsuarioRespuestas as
-select A.id as encuesta_id, C.usuario_id, count(C.usuario_id) as cantRespuestas, count(B.encuesta_id) as totalPreguntas, (count(C.usuario_id)/count(B.encuesta_id)) * 100  as porcentaje from encuestas.encuestas as A 
-inner join encuestas.encuestas_preguntas as B on A.id = B.encuesta_id 
-inner join encuestas.respuestas as C on (B.pregunta_id  = C.pregunta_id and C.encuesta_id = A.id ) group by A.id, C.usuario_id  order by C.usuario_id  DESC;
+CREATE OR REPLACE VIEW encuestas.resumen_usuario_respuestas AS 
+ SELECT a.id AS encuesta_id,
+    d.usuario_id as usuario_id,
+    count(e.usuario_id) AS cantrespuestas,
+    count(b.encuesta_id) AS totalpreguntas,
+    case when (count(b.encuesta_id) > 0)
+      then count(e.usuario_id) / count(b.encuesta_id) * 100
+      else
+	0 END AS porcentaje
+   FROM encuestas.encuestas a
+   LEFT  JOIN encuestas.encuestas_preguntas as b ON a.id = b.encuesta_id
+   LEFT  JOIN encuestas.encuestas_grupos as c ON c.encuesta_id = a.id
+   inner JOIN encuestas.grupos_usuarios  as d ON c.grupo_id = d.grupo_id
+   LEFT JOIN respuestas e ON b.pregunta_id = e.pregunta_id AND e.encuesta_id = a.id AND e.usuario_id = d.usuario_id
+  GROUP BY a.id,d.usuario_id
+  ORDER BY d.usuario_id DESC;
+
+ALTER TABLE resumen_usuario_respuestas
+  OWNER TO encuestas;
 
 
 CREATE OR REPLACE VIEW v_encuestas_preguntas AS 
