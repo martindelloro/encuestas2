26a4481dc9322669c8d29dea44b6115281396fa6
martin <martin@martin.(none)>
2014-09-09 22:32:42 -0300

    Ahora sí, empaquetado

--- app/webroot/js/graficos/pie.js
+++ app/webroot/js/graficos/pie.js
@@ -14,10 +14,25 @@ console.log(contenido);
 //console.log()
  */
 var pie = new d3pie("graficoPie", {
+    size: {
+		"canvasHeight": 600,
+		"canvasWidth": 750
+	},
     data: { 
        content: contenido
        
-    }
+    },
+    labels: {
+		"outer": {
+			"format": "label-percentage1",
+			"pieDistance": 32
+                },                  
+                
+		"percentage": {
+			"color": "#0f0e0e",
+			"fontSize": 10,
+			"decimalPlaces": 3
+		}}
 });
 /*svg.selectAll(".bar")
       .data(datos)
